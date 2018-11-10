34f20d8e4475300ab3656f6166237bfc4d7b46b5
Hamsting <qudgus6214@naver.com>
2018-11-10 16:17:03 +0900

    움직임 수정

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -13,6 +13,7 @@ namespace Hwalgong
         private Rigidbody rb;
         [SerializeField]
         private bool isGround = true;
+        
 
 
 
@@ -30,19 +31,20 @@ namespace Hwalgong
             Vector3 moveVec = new Vector3(xAxis, 0.0f, zAxis) * baseMoveSpeed * Time.deltaTime;
             target.Translate(moveVec, Space.Self);
             
-            Ray ray = new Ray(target.position, Vector3.down);
-            Debug.DrawRay(ray.origin, ray.direction * 0.2f);
+            Ray ray = new Ray(target.position + Vector3.up * 0.1f, Vector3.down);
             isGround = Physics.Raycast(ray, 0.2f, (1 << LayerMask.NameToLayer("Map")));
+
+            // if (isGround)
+            //     rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 rb.velocity = Vector3.zero;
+                
                 if (isGround)
-                    rb.AddForce(Vector3.up * baseJumpForce, ForceMode.Force);
+                    rb.AddForce(Vector3.up * baseJumpForce);
                 else
-                    rb.AddForce((target.forward + new Vector3(0.0f, 0.5f, 0.0f)).normalized * baseJumpForce, ForceMode.Force);
-
-                Debug.Log((target.forward + new Vector3(0.0f, 0.5f, 0.0f)).normalized);
+                    rb.AddForce((target.forward + Vector3.up) * baseJumpForce);
             }
         }
     }
