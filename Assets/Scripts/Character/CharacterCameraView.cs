b60c6fcb9b9106bae433c3f909cc665c201ea440
Hamsting <qudgus6214@naver.com>
2018-11-10 15:57:54 +0900

    에셋 추가, 이동 및 카메라뷰 제작

---
+++ Assets/Scripts/Character/CharacterCameraView.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hwalgong
+{
+    public class CharacterCameraView : MonoBehaviour
+    {
+        public Transform camPivot;
+        public Collider characterCollider;
+        public float baseRotateSpeed = 5.0f;
+        public float baseDistanceSpeed = 2.0f;
+        public bool isInversedX = false;
+        public bool isInversedY = false;
+
+        private Camera cam;
+        private Transform target;
+        private bool viewEnabled = true;
+        private float camDistance = 5.0f;
+        private float camAngle = 45.0f;
+
+
+
+        private void Awake()
+        {
+            cam = camPivot.GetComponentInChildren<Camera>();
+
+            if (characterCollider == null)
+                characterCollider = this.GetComponent<Collider>();
+
+            if (characterCollider != null)
+                target = characterCollider.transform;
+
+
+            viewEnabled = !viewEnabled;
+            Cursor.visible = !viewEnabled;
+            Cursor.lockState = (viewEnabled) ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                viewEnabled = !viewEnabled;
+                Cursor.visible = !viewEnabled;
+                Cursor.lockState = (viewEnabled) ? CursorLockMode.Locked : CursorLockMode.None;
+            }
+
+            float mouseWheel = (viewEnabled) ? Input.GetAxis("Mouse ScrollWheel") : 0.0f;
+            float mouseXAxis = (viewEnabled) ? Input.GetAxis("Mouse X") : 0.0f;
+            float mouseYAxis = (viewEnabled) ? Input.GetAxis("Mouse Y") : 0.0f;
+            if (isInversedX)
+                mouseXAxis *= -1.0f;
+            if (isInversedY)
+                mouseYAxis *= -1.0f;
+
+            camDistance += baseDistanceSpeed * mouseWheel;
+
+            camAngle = Mathf.Clamp(camAngle - mouseYAxis * baseRotateSpeed * Time.deltaTime, -89.0f, 89.0f);
+
+            Vector3 chaRot = target.eulerAngles;
+            chaRot.y += mouseXAxis * baseRotateSpeed * Time.deltaTime;
+            target.eulerAngles = chaRot;
+            
+            camPivot.position = characterCollider.bounds.center;
+            camPivot.eulerAngles = new Vector3(camAngle, chaRot.y, 0.0f);
+
+            Vector3 camPos = new Vector3(0.0f, 0.0f, -camDistance);
+
+            Ray ray = new Ray(camPivot.position, -cam.transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, camDistance, (1 << LayerMask.NameToLayer("Map"))))
+                camPos.z = -hit.distance + 0.1f;
+
+            cam.transform.localPosition = camPos;
+        }
+    }
+}
---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hwalgong
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        public float baseMoveSpeed = 3.0f;
+        public float baseJumpForce = 400.0f;
+
+        private Transform target;
+        private Rigidbody rb;
+        [SerializeField]
+        private bool isGround = true;
+
+
+
+        private void Awake()
+        {
+            target = this.transform;
+            rb = target.GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            float zAxis = Input.GetAxis("Vertical");
+            float xAxis = Input.GetAxis("Horizontal");
+
+            Vector3 moveVec = new Vector3(xAxis, 0.0f, zAxis) * baseMoveSpeed * Time.deltaTime;
+            target.Translate(moveVec, Space.Self);
+            
+            Ray ray = new Ray(target.position, Vector3.down);
+            Debug.DrawRay(ray.origin, ray.direction * 0.2f);
+            isGround = Physics.Raycast(ray, 0.2f, (1 << LayerMask.NameToLayer("Map")));
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.velocity = Vector3.zero;
+                if (isGround)
+                    rb.AddForce(Vector3.up * baseJumpForce, ForceMode.Force);
+                else
+                    rb.AddForce((target.forward + new Vector3(0.0f, 0.5f, 0.0f)).normalized * baseJumpForce, ForceMode.Force);
+
+                Debug.Log((target.forward + new Vector3(0.0f, 0.5f, 0.0f)).normalized);
+            }
+        }
+    }
+}
